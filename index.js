0b197cf02825f2345f1b4998ca77bcfb6c664fd6
kaja-shereef <157398343+kaja-shereef@users.noreply.github.com>
2024-01-28 14:30:38 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+// var button = document.getElementById("btn")
+//function lightmode(){
+  //  Element.style.backgroundColor= "#fff"
+
+   // }
+    //var button = document.getElementById("btn");
+
+    // function lightmode() {
+     // document.body.style.backgroundColor=("#fff")
+    // }
+
+    // var button = document.getElementById("#dark")
+    // function darkmode() {
+     // document.body.style.backgroundColor=("tan")
+    //}
+     function lightmode() {
+      document.body.classList.toggle
+      ("darkmode")
+     }
